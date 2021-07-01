c260122a172783aadf205393bcc2a05b9c49d037
Christian Gossain <cgossain@gmail.com>
2021-07-01 11:56:39 -0600

    Minor cleanup

--- FetchedResultsController/Classes/Persistent Store/Store Connector/CRUDStore.swift
+++ FetchedResultsController/Classes/Persistent Store/Store Connector/CRUDStore.swift
@@ -49,7 +49,9 @@ open class CRUDStore<ResultType: StoreResult, RequestType: StoreRequest>: StoreC
     open override func execute(_ query: StoreQuery<ResultType, RequestType>) {
         super.execute(query)
         
-        // leaving this as a thought: considering there could be multiple
+        // leaving this as a thought:
+        //
+        // considering there could be multiple
         // active queries attached to this store, would it instead make
         // more sense to insert existing draft changes into any new query
         // that is added instead of clearing them out? would a subclass
