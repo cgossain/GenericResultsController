a7fd91c27e6c74ac03627bf90e7a17947e776279
Christian Gossain <cgossain@gmail.com>
2022-03-16 23:34:45 -0600

    Add dependencies to Package.swift

--- Package.swift
+++ Package.swift
@@ -8,23 +8,30 @@ let package = Package(
     platforms: [.iOS(v12)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "GenericResultsController",
-            targets: ["GenericResultsController"]),
+        .library(name: "GenericResultsController", targets: ["GenericResultsController"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/cgossain/Debounce.git", .upToNextMajor(from: "1.5.0")),
+        .package(url: "https://github.com/jflinter/Dwifft", .upToNextMajor(from: "0.9.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GenericResultsController",
-            dependencies: []),
+            dependencies: [
+                .product(name: "Debounce", package: "Debounce"),
+                .product(name: "Dwifft", package: "Dwifft"),
+            ]
+        ),
         .testTarget(
             name: "GenericResultsControllerTests",
-            dependencies: ["GenericResultsController"]),
+            dependencies: [
+                "GenericResultsController"
+            ]
+        ),
     ],
     swiftLanguageVersions: [.v5]
 )
